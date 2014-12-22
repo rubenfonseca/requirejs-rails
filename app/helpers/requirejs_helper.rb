a0fcc5a3af1c4c6d7b275198abd570bb4451e88e
Vasiliy Ermolovich <younash@gmail.com>
2014-12-22 22:17:20 +0300

    Separate requirejs_config method.

--- app/helpers/requirejs_helper.rb
+++ app/helpers/requirejs_helper.rb
@@ -8,7 +8,8 @@ module RequirejsHelper
   mattr_accessor :_priority
   @@_priority = []
 
-  def requirejs_include_tag(name = nil, &block)
+  def requirejs_config(name = nil, &block)
+    html = ""
     requirejs = Rails.application.config.requirejs
 
     if requirejs.loader == :almond
@@ -16,11 +17,58 @@ module RequirejsHelper
       return almond_include_tag(name, &block)
     end
 
+    unless requirejs.run_config.empty?
+      run_config = requirejs.run_config.dup
+
+      unless _priority.empty?
+        run_config = run_config.dup
+        run_config[:priority] ||= []
+        run_config[:priority].concat _priority
+      end
+
+      if Rails.application.config.assets.digest
+        modules = requirejs.build_config['modules'].map { |m| requirejs.module_name_for m }
+
+        # Generate digestified paths from the modules spec
+        paths = {}
+        modules.each { |m| paths[m] = javascript_path(m).sub /\.js$/, '' }
+
+        if run_config.has_key? 'paths'
+          # Add paths for assets specified by full URL (on a CDN)
+          run_config['paths'].each do |k, v|
+            paths[k] = v if v.is_a?(Array) || v =~ /^https?:/
+          end
+        end
+
+        # Override user paths, whose mappings are only relevant in dev mode
+        # and in the build_config.
+        run_config['paths'] = paths
+      end
+
+      run_config['baseUrl'] = base_url(name)
+
+      html.concat(content_tag(:script) do
+        script = "require.config(#{run_config.to_json});"
+
+        # Pass an array to `require`, since it's a top-level module about to be loaded asynchronously (see
+        # `http://requirejs.org/docs/errors.html#notloaded`).
+        script.concat(" require([#{name.dump}]);") \
+          if name
+
+        script.html_safe
+      end)
+
+      html.html_safe
+    end
+  end
+
+  def requirejs_include_tag(name = nil, &block)
     html = ""
+    html.concat requirejs_config(name, &block)
 
     once_guard do
       rjs_attributes = {
-          src: javascript_path("require")
+        src: javascript_path("require")
       }
 
       rjs_attributes = rjs_attributes.merge(Hash[block.call(controller).map do |key, value|
@@ -29,48 +77,6 @@ module RequirejsHelper
         if block
 
       html.concat(content_tag(:script, "", rjs_attributes))
-
-      unless requirejs.run_config.empty?
-        run_config = requirejs.run_config.dup
-
-        unless _priority.empty?
-          run_config = run_config.dup
-          run_config[:priority] ||= []
-          run_config[:priority].concat _priority
-        end
-
-        if Rails.application.config.assets.digest
-          modules = requirejs.build_config['modules'].map { |m| requirejs.module_name_for m }
-
-          # Generate digestified paths from the modules spec
-          paths = {}
-          modules.each { |m| paths[m] = javascript_path(m).sub /\.js$/, '' }
-
-          if run_config.has_key? 'paths'
-            # Add paths for assets specified by full URL (on a CDN)
-            run_config['paths'].each do |k, v|
-              paths[k] = v if v.is_a?(Array) || v =~ /^https?:/
-            end
-          end
-
-          # Override user paths, whose mappings are only relevant in dev mode
-          # and in the build_config.
-          run_config['paths'] = paths
-        end
-
-        run_config['baseUrl'] = base_url(name)
-
-        html.concat(content_tag(:script) do
-          script = "require.config(#{run_config.to_json});"
-
-          # Pass an array to `require`, since it's a top-level module about to be loaded asynchronously (see
-          # `http://requirejs.org/docs/errors.html#notloaded`).
-          script.concat(" require([#{name.dump}]);") \
-            if name
-
-          script.html_safe
-        end)
-      end
 
       html.html_safe
     end
